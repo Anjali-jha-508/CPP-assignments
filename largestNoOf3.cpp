9ec0a5718738ca275d53063a7849137fe1028b87
Anjali Jha <anjali.jha.x.858@gmail.com>
2026-09-26 16:46:53 +0530

    Add files via upload

---
+++ largestNoOf3.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+using namespace std;  
+
+int main() {     
+    double num1, num2, num3;      // Taking input from the user  
+
+    cout << "Enter number 1: ";     
+    cin >> num1;     
+    cout << "Enter number 2: ";     
+    cin >> num2;     
+    cout << "Enter number 3: ";     
+    cin >> num3;  
+        
+    // Checking conditions to find the largest number   
+    
+    //number 1 is greater than or equal to both number 2 and number 3
+    if (num1 >= num2 && num1 >= num3) {         
+        cout << "The largest number is: " << num1 << endl;     
+    }     
+    //number 2 is greater than or equal to both number 1 and number 3
+    else if (num2 >= num1 && num2 >= num3) {         
+        cout << "The largest number is: " << num2 << endl;     
+    }  
+    //number 3 is greater than or equal to both number 1 and number 2   
+    else {         
+        cout << "The largest number is: " << num3 << endl;     
+    }      
+
+    return 0; 
+}  
