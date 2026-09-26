bf94af2e18cb4d21d5e0976aff671df461eb4228
Anjali Jha <anjali.jha.x.858@gmail.com>
2026-09-26 19:28:46 +0530

    Refactor rectangle class and update method names

--- program4-1.cpp
+++ program4-1.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class rectangle {
-private: 
-	float length, breadth, area, perimeter;
-public:
-	void getDimensions() {
-	cout << "Enter length of Rectangle: ";
-	cin >> length;
-	cout << "Enter breadth of Rectangle: ";
-	cin >> breadth;
-}
-	void calcArea() {
-	area = length*breadth;
-}
-void calcPerimeter() {
-	perimeter = 2*(length+breadth);
-}
-void display() {
-cout << "Length is: " << length << endl;
-cout << "Breadth is: " << breadth << endl;
-cout << "Area is: " << area << endl;
-cout << "Perimeter is: " << perimeter << endl;
-}
+
+class Rectangle {
+    private:
+        float length, breadth, area, perimeter;
+    public:
+        void getDimensions() {
+            cout << "Enter length of rectangle: ";
+            cin >> length;
+            cout << "Enter breadth of rectangle: ";
+            cin >> breadth;
+        }
+        void calculateArea() {
+            area = length * breadth;
+        }
+        void calculatePerimeter() {
+            perimeter = 2 * (length + breadth);
+        }
+        void displayResults() {
+            cout << "----Rectangle Details----" << endl;
+            cout << "Length: " << length << " units" << endl;
+            cout << "Breadth: " << breadth << " units" << endl;
+            cout << "Area: " << area << " square units" << endl;
+            cout << "Perimeter: " << perimeter << " units" << endl;
+        }
 };
 
 int main() {
-rectangle r1;
-r1.getDimensions();
-r1.calcArea();
-r1.calcPerimeter();
-r1.display();
+    cout << "To Create a Rectangle class with private data members" << endl << "and define member functions both inside the class" << endl << "to calculate area and perimeter" << endl;
+    cout << "---RECTANGLE AREA AND PERIMETER CALCULATOR---" << endl;
 
-return 0;
+    Rectangle r1;
+    r1.getDimensions();
+    r1.calculateArea();
+    r1.calculatePerimeter();
+    r1.displayResults();
+
+    return 0;
 }
